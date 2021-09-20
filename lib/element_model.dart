69ff06d2bc51832cc7b28ad406ffd46c93215f39
Denis <denis.nadey@gmail.com>
2021-09-20 18:48:07 +0300

    minor update

--- lib/element_model.dart
+++ lib/element_model.dart
@@ -1,12 +1,40 @@
+enum ElementCardImages {
+  rastvor,
+  beton,
+  gidrobeton,
+  betonanasos,
+  jbi,
+  bloc_build,
+  ascel,
+  luki,
+  luki_chugun,
+  develop
+}
+
 class ElementCard {
   late String categoryId;
   late String categoryName;
   late String type;
   late List<CategoryList> categoryList;
+
+  final List<String> pathImageUrl = [
+    "https://chernomorbeton.ru/wp-content/uploads/2018/11/kupit-cementnyj-rastvor-v-anape-i-anapskom-rajone.jpg", //rastvor,
+    "https://chernomorbeton.ru/wp-content/uploads/2018/11/kupit-beton-v-anape-i-anapskom-rajone.jpg", // beton
+    "https://chernomorbeton.ru/wp-content/uploads/2018/11/kupit-gidrotexnicheskij-beton-v-anape-i-anapskom-rajone.jpg", // gidrobeton
+    "https://chernomorbeton.ru/wp-content/uploads/2018/11/1522152740_arenda_betononasosa_2.jpg",
+    "https://chernomorbeton.ru/wp-content/uploads/2019/04/zavod-zhbi-oficialnyj-sajt-anapa-novorossijsk.jpg", // jbi
+    "https://chernomorbeton.ru/wp-content/uploads/2019/04/proizvodstvo-stroitelnyx-blokov-1.jpg", //bloc_build
+    "https://chernomorbeton.ru/wp-content/uploads/2019/04/asbestocementnye-truby-anapa-novorossijsk.jpg", // ascel
+    "https://chernomorbeton.ru/wp-content/uploads/2019/04/kupit-plastikovyj-kanalizacionnyj-lyuk-anapa-novorossijsk.jpg", //luki,
+    "https://chernomorbeton.ru/wp-content/uploads/2019/04/lyuk-kanalizacionnyj-chugunnyj-anapa-novorossijsk.jpg", // luki_chugun
+    "https://chernomorbeton.ru/wp-content/uploads/2019/12/Рытье-котлована-с-перевозкой-грунта.jpg", //develop
+  ];
+  late String imageuri;
   ElementCard(
       {required this.categoryId,
       required this.categoryName,
       required this.type,
+      required this.imageuri,
       required this.categoryList});
 
   ElementCard.fromJson(Map<String, dynamic> json) {
@@ -19,6 +47,32 @@ class ElementCard {
         categoryList.add(CategoryList.fromJson(v));
       });
     }
+  }
+  String getImageById() {
+    switch (this.categoryId) {
+      case "36":
+        return pathImageUrl[ElementCardImages.rastvor.index];
+      case "42":
+        return pathImageUrl[ElementCardImages.beton.index];
+      case "52":
+        return pathImageUrl[ElementCardImages.gidrobeton.index];
+      case "58":
+        return pathImageUrl[ElementCardImages.betonanasos.index];
+      case "64":
+        return pathImageUrl[ElementCardImages.jbi.index];
+      case "81":
+        return pathImageUrl[ElementCardImages.bloc_build.index];
+      case "94":
+        return pathImageUrl[ElementCardImages.ascel.index];
+      case "110":
+        return pathImageUrl[ElementCardImages.luki.index];
+      case "120":
+        return pathImageUrl[ElementCardImages.luki_chugun.index];
+      case "124":
+        return pathImageUrl[ElementCardImages.develop.index];
+    }
+
+    return "https://chernomorbeton.ru/wp-content/uploads/2018/11/about-us.jpg";
   }
 }
 
--- lib/elements.dart
+++ lib/elements.dart
@@ -23,50 +23,162 @@ class EntityPageState extends State<ElementCardsPage> {
         appBar: AppBar(
           title: Text("CHBD✌️"),
         ),
+        backgroundColor: Color(0xff43515a),
         body: Center(
             child: Padding(
-          padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
+          padding:
+              const EdgeInsets.only(top: 16, left: 30, right: 30, bottom: 16),
           child: Center(child: BlocBuilder<EntityBloc, EntityState>(
             builder: (context, state) {
               if (state is EntityEmpty) {
                 return Text(state.toString());
               } else if (state is EntityLoaded) {
                 return ListView.separated(
+                  clipBehavior: Clip.none,
                   itemCount: state.elements.length,
                   itemBuilder: (BuildContext context, int index) {
                     ElementCard _element = state.elements[index];
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          title: Text(_element.categoryName),
-                          subtitle: Text(_element.type),
-                        ),
-                        Text(
-                          "Выбери наименование: ",
-                          textAlign: TextAlign.start,
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: ListView.builder(
-                            itemCount: _element.categoryList.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(_element.categoryList[index].id +
-                                    " | " +
-                                    _element.categoryList[index].name +
-                                    " | " +
-                                    _element.categoryList[index].price +
-                                    " | " +
-                                    _element.type),
-                              );
-                            },
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(11),
+                        color: Color(0xc1f7f7f7),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 14,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 68,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(13),
+                                  ),
+                                  child: Container(
+                                    clipBehavior: Clip.hardEdge,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(13),
+                                      color: Colors.white,
+                                    ),
+                                    child: Image.network(
+                                      _element.getImageById(),
+                                      fit: BoxFit.fill,
+                                      width: 68,
+                                      height: 68,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 22),
+                                Container(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: SizedBox(
+                                          width: 163,
+                                          child: Text(
+                                            _element.categoryName,
+                                            style: TextStyle(
+                                              color: Color(0xff3b5779),
+                                              fontSize: 17,
+                                              fontFamily: "Roboto",
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 6),
+                                      Container(
+                                        height: 41,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Единица измерения - ${_element.type}",
+                                              style: TextStyle(
+                                                color: Color(0xff3b5779),
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                            SizedBox(height: 8),
+                                            Container(
+                                              height: 19,
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Container(
+                                                    height: 19,
+                                                    decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              9),
+                                                      color: Colors.white,
+                                                    ),
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                      horizontal: 18,
+                                                      vertical: 3,
+                                                    ),
+                                                    child: Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .center,
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .center,
+                                                      children: [
+                                                        Text(
+                                                          "Выберите наименование",
+                                                          style: TextStyle(
+                                                            color: Color(
+                                                                0xff3b5779),
+                                                            fontSize: 11,
+                                                            fontFamily:
+                                                                "Roboto",
+                                                            fontWeight:
+                                                                FontWeight.w300,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
-                      ],
+                        ],
+                      ),
                     );
                   },
-                  separatorBuilder: (context, index) => Divider(),
+                  separatorBuilder: (context, index) => SizedBox(
+                    height: 16,
+                  ),
                 );
               } else if (state is EntityLoading) {
                 return CircularProgressIndicator(
@@ -86,6 +198,50 @@ class EntityPageState extends State<ElementCardsPage> {
           backgroundColor: Colors.blue,
         ),
       ),
+    );
+  }
+}
+
+class CardOldChbd extends StatelessWidget {
+  const CardOldChbd({
+    Key? key,
+    required ElementCard element,
+  })  : _element = element,
+        super(key: key);
+
+  final ElementCard _element;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          title: Text(_element.categoryName),
+          subtitle: Text(_element.type),
+        ),
+        Text(
+          "Выбери наименование: ",
+          textAlign: TextAlign.start,
+        ),
+        SizedBox(
+          height: 300,
+          child: ListView.builder(
+            itemCount: _element.categoryList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_element.categoryList[index].id +
+                    " | " +
+                    _element.categoryList[index].name +
+                    " | " +
+                    _element.categoryList[index].price +
+                    " | " +
+                    _element.type),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
